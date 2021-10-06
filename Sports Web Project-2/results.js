d2c82152d139aaaebdf75d6edbf381fee7795f1c
Kabilan-Sekar <85468996+Kabilan-Sekar@users.noreply.github.com>
2021-10-06 20:51:09 +0530

    Add files via upload

---
+++ Sports Web Project-2/results.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyAFQe_1yur4OHLtC4S2MlhZeOW1sflomPE",
+    authDomain: "ipl-results-93cf1.firebaseapp.com",
+    databaseURL: "https://ipl-results-93cf1-default-rtdb.firebaseio.com",
+    projectId: "ipl-results-93cf1",
+    storageBucket: "ipl-results-93cf1.appspot.com",
+    messagingSenderId: "532224893266",
+    appId: "1:532224893266:web:d884106552cddb95a31a8b"
+}
+
+firebaseConfig.initializeApp(firebaseConfig)
+
